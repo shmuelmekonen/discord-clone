33f2aaf7c9552512a7ba6d8f59029ce175dd4bae
Shmuel Mekonen <shmuelmekonen@gmail.com>
2025-12-23 20:22:54 +0200

    Added Api to Create a server at api/server/route.ts

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Test = () => {
-  return <div>Test</div>;
+  return (
+    <div>
+      Test <UserButton />
+    </div>
+  );
 };
 
 export default Test;
--- components/models/initial-model.tsx
+++ components/models/initial-model.tsx
@@ -56,7 +56,6 @@ export const InitialModel = () => {
 
     defaultValues: {
       name: "",
-
       imageUrl: "",
     },
   });
