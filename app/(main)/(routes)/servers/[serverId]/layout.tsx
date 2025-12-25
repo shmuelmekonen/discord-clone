5140890bfe4802b85d6f540d9c707ba4d5dc1da4
Shmuel Mekonen <shmuelmekonen@gmail.com>
2025-12-25 20:18:42 +0200

    Finished Server /layout.tsx, Added server-header and server-sidebar

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,3 +1,4 @@
+import { ServerSidebar } from "@/components/server/server-sidebar";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
@@ -31,10 +32,11 @@ const ServerIdLayout = async ({
   if (!server) redirect("/");
 
   return (
-    <div className="h-full ">
+    <div className="h-full">
       <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-        <main className="h-full md:pl-60">{children}</main>
+        <ServerSidebar serverId={serverId} />
       </div>
+      <main className="h-full md:pl-60">{children}</main>
     </div>
   );
 };
