7f4ab58f60bc525ead3215118fae2768886dc938
Shmuel Mekonen <shmuelmekonen@gmail.com>
2026-01-13 14:33:20 +0200

    feat(ui): scaffold server sidebar with search and text channels structure

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -9,6 +9,9 @@ import { db } from "@/lib/db";
 import { ServerHeader } from "@/components/server/server-header";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { ServerSearch } from "@/components/server/server-search";
+import { Separator } from "@/components/ui/separator";
+import ServerSection from "@/components/server/server-section";
+import ServerChannel from "./server-channel";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -121,6 +124,25 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
             ]}
           />
         </div>
+        <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
+        {!!textChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={ChannelType.TEXT}
+              role={role}
+              label="Text Channels"
+            />
+            {textChannels.map((channel) => (
+              <ServerChannel
+                key={channel.id}
+                channel={channel}
+                role={role}
+                server={server}
+              />
+            ))}
+          </div>
+        )}
       </ScrollArea>
     </div>
   );
