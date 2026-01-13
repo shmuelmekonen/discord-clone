175d1e721e32e9cd84b429f7ca7bdcec9e74852b
Shmuel Mekonen <shmuelmekonen@gmail.com>
2026-01-13 13:32:00 +0200

    feat(ui): add navigation logic to server search command palette

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -10,6 +10,7 @@ import {
   CommandItem,
   CommandList,
 } from "@/components/ui/command";
+import { useParams, useRouter } from "next/navigation";
 
 interface ServerSearchProps {
   data: {
@@ -26,6 +27,8 @@ interface ServerSearchProps {
 }
 export const ServerSearch = ({ data }: ServerSearchProps) => {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const params = useParams();
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
@@ -42,6 +45,22 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
     document.addEventListener("keydown", down);
     return () => document.removeEventListener("keydown", down);
   }, []);
+
+  const onClick = ({
+    id,
+    type,
+  }: {
+    id: string;
+    type: "channel" | "member";
+  }) => {
+    setOpen(false);
+
+    if (type === "member")
+      return router.push(`/servers/${params?.serverId}/conversations/${id}`);
+
+    if (type === "channel")
+      return router.push(`/servers/${params?.serverId}/channels/${id}`);
+  };
 
   return (
     <>
@@ -68,7 +87,10 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
               <CommandGroup key={label} heading={label}>
                 {data.map(({ id, icon, name }) => {
                   return (
-                    <CommandItem key={id}>
+                    <CommandItem
+                      key={id}
+                      onSelect={() => onClick({ id, type })}
+                    >
                       {icon}
                       <span>{name}</span>
                     </CommandItem>
