b0f9fee6ceceb7db44c4b66600d250d7d8f2406f
Shmuel Mekonen <shmuelmekonen@gmail.com>
2026-01-14 10:14:14 +0200

    feat(ui): add channel action buttons (edit/delete) and lock icon for general

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -3,8 +3,9 @@
 import { CHANNEL_NAMES } from "@/lib/constants";
 import { cn } from "@/lib/utils";
 import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
-import { Hash, Mic, Video } from "lucide-react";
+import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { ActionTooltip } from "@/components/action-tooltop";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -41,6 +42,19 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
       >
         {channel.name}
       </p>
+      {channel.name !== CHANNEL_NAMES.GENERAL && role !== MemberRole.GUEST && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <ActionTooltip label="Edit">
+            <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+          </ActionTooltip>
+          <ActionTooltip label="Delete">
+            <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+          </ActionTooltip>
+        </div>
+      )}
+      {channel.name === CHANNEL_NAMES.GENERAL && (
+        <Lock className="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400" />
+      )}
     </button>
   );
 };
