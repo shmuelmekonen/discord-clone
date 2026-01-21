427a81a33f2e68c764e3c40662103c5e54a6052b
Shmuel Mekonen <shmuelmekonen@gmail.com>
2026-01-22 01:22:59 +0200

    feat(server): implement smart initial channel redirect

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,17 +1,43 @@
 import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-const ServerIDPage = async () => {
-  //Remove async
-  const profile = await currentProfile(); //Remove
-  if (profile)
-    return (
-      <div>
-        Server ID Page
-        <br /> {/*//Remove*/}
-        {profile.name}
-        {/*//Remove*/}
-      </div>
-    );
+interface ServerIDPageProps {
+  params: Promise<{
+    serverId: string;
+  }>;
+}
+const ServerIDPage = async ({ params }: ServerIDPageProps) => {
+  const profile = await currentProfile();
+  // TODO: Fix redirect to sign-in handler.
+  if (!profile) redirect("/sign-in");
+
+  const { serverId } = await params;
+
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        orderBy: { createdAt: "asc" },
+        take: 1,
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (!server || !initialChannel) {
+    return redirect("/");
+  }
+
+  return redirect(`/servers/${serverId}/channels/${initialChannel.id}`);
 };
 
 export default ServerIDPage;
