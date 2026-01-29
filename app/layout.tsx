9dec16936c98f3d2a378e2a35c460e8794dcf05a
Shmuel Mekonen <shmuelmekonen@gmail.com>
2026-01-29 18:50:51 +0200

    chore(deps): install and configure tanstack query

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import { Toaster } from "sonner";
 
 import "./globals.css";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -48,7 +49,7 @@ export default async function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>{children}</QueryProvider>
               <Toaster richColors position="bottom-right" />
             </SocketProvider>
           </ThemeProvider>
